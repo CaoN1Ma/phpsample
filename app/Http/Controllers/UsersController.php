9dfe2869b8645b7905b0e49faec0b50ca3bea3dc
john <kyle3278@msn.com>
2018-11-19 08:05:52 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,18 +9,31 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except'=>['create','store','index']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+//        只允许未登录用户注册
+    }
+
     public function create()
     {
         return view('users.create');
     }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
-    public function store(request $request)
+
+    public function store(Request $request)
     {
         $this->validate($request,[
-            'name'=>'required|max:50',
+            'name'=>'required|unique:users|max:50',
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
@@ -33,5 +46,47 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success','注册成功，欢迎');
         return redirect()->route('users.show',[$user]);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+
+        $this->validate($request,[
+            'name'=>'required|unique:users|max:50',
+            'password'=>'nullable|confirmed|min:6'
+
+        ]);
+
+        $this->authorize('update', $user);
+
+        $data=[];
+        $data['name']=$request->name;
+        if ($request->password){
+            $data['password']=bcrypt($request->password);
+        }
+
+        $user->update($data);
+        session()->flash('success','更新成功');
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function index()
+    {
+        $users =User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success','删除成功');
+        return back();
     }
 }
